4d4e714564a240522636630bf68e7d73e07c1556
Lucas Silva <lucas-silva-98@hotmail.com>
2024-02-02 14:40:55 -0300

    feat: Implementação de split do PDF com sucesso

--- ManipulandoArquivoPratica/Controllers/HomeController.cs
+++ ManipulandoArquivoPratica/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using iText.IO.Source;
 using iText.Kernel.Pdf;
 using ManipulandoArquivoPratica.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -25,35 +26,35 @@ namespace ManipulandoArquivoPratica.Controllers
         {
             return View();
         }
+
         //MÉTODO QUE COPIA 1 PDF E GERA UM NOVO COM SUCESSO
+        [HttpPost]
+        public IActionResult SplitAntigo(IFormFile pdf)
+        {
+            try
+            {
+                // Criando um espaço na memória para a gravação de um PDF
+                var zipMemoryStream = new MemoryStream();
+                var pdfWriter = new PdfWriter(zipMemoryStream);
+                var pdfDocument = new PdfDocument(pdfWriter);
 
-        //[HttpPost]
-        //public IActionResult Split(IFormFile pdf)
-        //{
-        //    try
-        //    {
-        //        // Criando um espaço na memória para a gravação de um PDF
-        //        var zipMemoryStream = new MemoryStream();
-        //        var pdfWriter = new PdfWriter(zipMemoryStream);
-        //        var pdfDocument = new PdfDocument(pdfWriter);
+                // Obtendo o conteúdo do pdf
+                var stream = pdf.OpenReadStream();
+                var pdfReader = new PdfReader(stream);
+                var pdfDocumentReader = new PdfDocument(pdfReader);
+                pdfDocumentReader.CopyPagesTo(1, 1, pdfDocument);
 
-        //        // Obtendo o conteúdo do pdf
-        //        var stream = pdf.OpenReadStream();
-        //        var pdfReader = new PdfReader(stream);
-        //        var pdfDocumentReader = new PdfDocument(pdfReader);
-        //        pdfDocumentReader.CopyPagesTo(1, 1, pdfDocument);
+                pdfDocumentReader.Close();
+                pdfDocument.Close();
 
-        //        pdfDocumentReader.Close();
-        //        pdfDocument.Close();
-
-        //        var conteudoPDF = zipMemoryStream.ToArray();
-        //        return File(conteudoPDF, "application/pdf", "nome_do_arquivo.pdf");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw new Exception(ex.Message);
-        //    }
-        //}
+                var conteudoPDF = zipMemoryStream.ToArray();
+                return File(conteudoPDF, "application/pdf", "nome_do_arquivo.pdf");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
         [HttpPost]
         public IActionResult Split(IFormFile pdf)
@@ -71,31 +72,50 @@ namespace ManipulandoArquivoPratica.Controllers
                         var pdfMemoryStream = new MemoryStream();
                         var pdfWriter = new PdfWriter(pdfMemoryStream);
                         var pdfDocument = new PdfDocument(pdfWriter);
+                        //pdfMemoryStream.Position = 0;
 
                         // Obtendo o conteúdo do pdf
                         var stream = pdf.OpenReadStream();
                         var pdfReader = new PdfReader(stream);
+                        stream.Position = 0;
                         var pdfDocumentReader = new PdfDocument(pdfReader);
+
                         pdfDocumentReader.CopyPagesTo(page, page, pdfDocument);
+
+                        
 
                         // Adicionando o PDF específico ao arquivo ZIP
                         var entry = zipArchive.CreateEntry($"pagina_{page}.pdf");
                         using (var entryStream = entry.Open())
                         {
+                            var writ = new PdfWriter(entryStream);
+                            var doc = new PdfDocument(writ);
+
                             pdfMemoryStream.Position = 0;
+                            pdfMemoryStream.Seek(0, SeekOrigin.Begin);
                             pdfMemoryStream.CopyTo(entryStream);
+                            pdfDocumentReader.CopyPagesTo(page, page, doc);
+
+                            doc.Close();
                         }
 
-                        pdfDocument.Close();
+
                         pdfDocumentReader.Close();
-                        pdfWriter.Close();
                         pdfReader.Close();
                         stream.Close();
+                        pdfDocument.Close();
+                        pdfWriter.Close();
+                        pdfMemoryStream.Close();
                     }
+              
                 }
 
                 zipMemoryStream.Position = 0;
+                zipMemoryStream.Seek(0, SeekOrigin.Begin);
+
                 var conteudoZIP = zipMemoryStream.ToArray();
+
+                //zipMemoryStream.Close();
 
                 return File(conteudoZIP, "application/zip", "nome_do_arquivo.zip");
             }
@@ -111,6 +131,9 @@ namespace ManipulandoArquivoPratica.Controllers
             var stream = pdf.OpenReadStream();
             var reader = new PdfReader(stream);
             var doc = new PdfDocument(reader);
+
+            stream.Close();
+            reader.Close();
 
             return doc.GetNumberOfPages();
         }
