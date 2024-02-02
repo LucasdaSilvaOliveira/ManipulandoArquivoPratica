8c0ab27f5355c81951473298f6c30999c47523d2
Lucas Silva <lucas-silva-98@hotmail.com>
2024-02-01 22:33:50 -0300

    feat: Implementação de Split do PDF

--- ManipulandoArquivoPratica/Controllers/HomeController.cs
+++ ManipulandoArquivoPratica/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using iText.Kernel.Pdf;
 using ManipulandoArquivoPratica.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.IO.Compression;
+using System.Reflection.Metadata;
 
 namespace ManipulandoArquivoPratica.Controllers
 {
@@ -23,36 +25,94 @@ namespace ManipulandoArquivoPratica.Controllers
         {
             return View();
         }
+        //MÉTODO QUE COPIA 1 PDF E GERA UM NOVO COM SUCESSO
+
+        //[HttpPost]
+        //public IActionResult Split(IFormFile pdf)
+        //{
+        //    try
+        //    {
+        //        // Criando um espaço na memória para a gravação de um PDF
+        //        var zipMemoryStream = new MemoryStream();
+        //        var pdfWriter = new PdfWriter(zipMemoryStream);
+        //        var pdfDocument = new PdfDocument(pdfWriter);
+
+        //        // Obtendo o conteúdo do pdf
+        //        var stream = pdf.OpenReadStream();
+        //        var pdfReader = new PdfReader(stream);
+        //        var pdfDocumentReader = new PdfDocument(pdfReader);
+        //        pdfDocumentReader.CopyPagesTo(1, 1, pdfDocument);
+
+        //        pdfDocumentReader.Close();
+        //        pdfDocument.Close();
+
+        //        var conteudoPDF = zipMemoryStream.ToArray();
+        //        return File(conteudoPDF, "application/pdf", "nome_do_arquivo.pdf");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception(ex.Message);
+        //    }
+        //}
 
         [HttpPost]
         public IActionResult Split(IFormFile pdf)
         {
             try
             {
-                // Criando um espaço na memória para a gravação de um PDF
                 var zipMemoryStream = new MemoryStream();
-                var pdfWriter = new PdfWriter(zipMemoryStream);
-                var pdfDocument = new PdfDocument(pdfWriter);
+                using (var zipArchive = new ZipArchive(zipMemoryStream, ZipArchiveMode.Create, true))
+                {
+                    var pageCount = ObterQuantidadePaginas(pdf);
 
-                // Obtendo o conteúdo do pdf
-                var stream = pdf.OpenReadStream();
-                var pdfReader = new PdfReader(stream);
-                var pdfDocumentReader = new PdfDocument(pdfReader);
-                pdfDocumentReader.CopyPagesTo(1, 1, pdfDocument);
+                    for (var page = 1; page <= pageCount; page++)
+                    {
+                        // Criando um espaço na memória para a gravação de um PDF específico
+                        var pdfMemoryStream = new MemoryStream();
+                        var pdfWriter = new PdfWriter(pdfMemoryStream);
+                        var pdfDocument = new PdfDocument(pdfWriter);
 
-                pdfDocumentReader.Close();
-                pdfDocument.Close();
+                        // Obtendo o conteúdo do pdf
+                        var stream = pdf.OpenReadStream();
+                        var pdfReader = new PdfReader(stream);
+                        var pdfDocumentReader = new PdfDocument(pdfReader);
+                        pdfDocumentReader.CopyPagesTo(page, page, pdfDocument);
 
-                var conteudoPDF = zipMemoryStream.ToArray();
+                        // Adicionando o PDF específico ao arquivo ZIP
+                        var entry = zipArchive.CreateEntry($"pagina_{page}.pdf");
+                        using (var entryStream = entry.Open())
+                        {
+                            pdfMemoryStream.Position = 0;
+                            pdfMemoryStream.CopyTo(entryStream);
+                        }
 
+                        pdfDocument.Close();
+                        pdfDocumentReader.Close();
+                        pdfWriter.Close();
+                        pdfReader.Close();
+                        stream.Close();
+                    }
+                }
 
-                return File(conteudoPDF, "application/pdf", "nome_do_arquivo.pdf");
+                zipMemoryStream.Position = 0;
+                var conteudoZIP = zipMemoryStream.ToArray();
+
+                return File(conteudoZIP, "application/zip", "nome_do_arquivo.zip");
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
+        }
 
+
+        private int ObterQuantidadePaginas(IFormFile pdf)
+        {
+            var stream = pdf.OpenReadStream();
+            var reader = new PdfReader(stream);
+            var doc = new PdfDocument(reader);
+
+            return doc.GetNumberOfPages();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
